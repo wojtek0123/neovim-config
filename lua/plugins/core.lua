b9f5311ba831e0bfe2987ac6678752c2ea589b01
Wojtek <wojtekpietraszuk@gmail.com>
2024-03-16 10:24:56 +0100

    Update config

---
+++ lua/plugins/core.lua
@@ -0,0 +1,90 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+-- if true then return {} end
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
+  -- Configure LazyVim to load gruvbox
+  -- {
+  --  "LazyVim/LazyVim",
+  --  opts = {
+  --    colorscheme = "kanagawa",
+  --  },
+  -- },
+
+  -- disable trouble
+  -- { "folke/trouble.nvim", enabled = false },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- add symbols-outline
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+  },
+
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "go",
+        "gomod",
+        "gowork",
+        "gosum",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "rust",
+        "css",
+        "vue",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+
+  -- add any tools you want to have installed below
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "nxls",
+      },
+    },
+  },
+
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+}
