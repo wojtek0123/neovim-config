ac20606f83bb6727c1b51a612d7fb006ea4f2842
Wojtek <wojtekpietraszuk@gmail.com>
2024-06-07 23:30:30 +0200

    Add inline template highlights and disable word highlight on cursor

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -114,28 +114,28 @@ return { -- LSP Configuration & Plugins
         --
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
         local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
-          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.document_highlight,
-          })
-
-          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-            buffer = event.buf,
-            group = highlight_augroup,
-            callback = vim.lsp.buf.clear_references,
-          })
-
-          vim.api.nvim_create_autocmd('LspDetach', {
-            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
-            callback = function(event2)
-              vim.lsp.buf.clear_references()
-              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
-            end,
-          })
-        end
+        -- if client and client.server_capabilities.documentHighlightProvider then
+        --   local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+        --   vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        --     buffer = event.buf,
+        --     group = highlight_augroup,
+        --     callback = vim.lsp.buf.document_highlight,
+        --   })
+        --
+        --   vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        --     buffer = event.buf,
+        --     group = highlight_augroup,
+        --     callback = vim.lsp.buf.clear_references,
+        --   })
+        --
+        --   vim.api.nvim_create_autocmd('LspDetach', {
+        --     group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+        --     callback = function(event2)
+        --       vim.lsp.buf.clear_references()
+        --       vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+        --     end,
+        --   })
+        -- end
 
         -- The following autocommand is used to enable inlay hints in your
         -- code, if the language server you are using supports them
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -24,13 +24,13 @@ return { -- Highlight, edit, and navigate code
     -- Autoinstall languages that are not installed
     auto_install = true,
     highlight = {
-      enable = false,
+      enable = true,
+      use_languagetree = true,
       -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
     },
-    indent = { enable = true, disable = { 'ruby' } },
+    indent = { enable = true },
   },
   config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
