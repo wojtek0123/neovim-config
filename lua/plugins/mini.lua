6ecbf393cdcc57e312323e0f52d3402d2293f6f6
Wojciech Pietraszuk <wpietraszuk@cloudferro.com>
2024-12-20 10:44:08 +0100

    Added buffer count to statusline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -38,6 +38,41 @@ return { -- Collection of various small independent plugins/modules
     -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
 
+    -- Function to get the full path and replace the home directory with ~
+    local function get_winbar_path()
+      local full_path = vim.fn.expand '%:p'
+      return full_path:gsub(vim.fn.expand '$HOME', '~')
+    end
+    -- Function to get the number of open buffers using the :ls command
+    local function get_buffer_count()
+      local buffers = vim.fn.execute 'ls'
+      local count = 0
+      -- Match only lines that represent buffers, typically starting with a number followed by a space
+      for line in string.gmatch(buffers, '[^\r\n]+') do
+        if string.match(line, '^%s*%d+') then
+          count = count + 1
+        end
+      end
+      return count
+    end
+    -- Function to update the winbar
+    local function update_winbar()
+      local home_replaced = get_winbar_path()
+      local buffer_count = get_buffer_count()
+
+      local path = '%#WinBar2#(' .. buffer_count .. ') ' .. '%#WinBar1#' .. home_replaced .. '%*%=%#WinBar2#'
+
+      -- Override the section_filename function to disable file path
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_filename = function()
+        return path
+      end
+    end
+    -- Autocmd to update the winbar on BufEnter and WinEnter events
+    vim.api.nvim_create_autocmd({ 'BufEnter', 'WinEnter' }, {
+      callback = update_winbar,
+    })
+
     -- You can configure sections in the statusline by overriding their
     -- default behavior. For example, here we set the section for
     -- cursor location to LINE:COLUMN
